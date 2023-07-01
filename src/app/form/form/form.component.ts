b2370e8f6fce80cc76f4bf4e43322ac081cd4249
OliverYR03 <oliver_del22@hotmail.com>
2023-07-01 12:21:19 -0500

    first commit

---
+++ src/app/form/form/form.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-form',
+  templateUrl: './form.component.html',
+  styleUrls: ['./form.component.css']
+})
+export class FormComponent implements OnInit {
+
+step: any = 1;
+
+constructor() {}
+
+ngOnInit(): void {
+
+}
+
+submit() {
+  this.step = this.step + 1;
+}
+}
